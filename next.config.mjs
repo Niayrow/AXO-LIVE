84c0c1b2ff52dec8c2b436791d8f1a5f42eaf029
SofianeWeb <soso.60@live.fr>
2026-05-29 14:40:25 +0200

    pwa ready

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
 };
 
-export default nextConfig;
+const withPWA = withPWAInit({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development", // Disable PWA in development to prevent caching issues
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+});
+
+export default withPWA(nextConfig);
